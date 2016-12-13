67ad560b76ec8e80e22dc08731c1e2aceb79af15
Gillesan <ansayg@isl-edu.be>
2016-12-13 20:53:21 +0100

    intermate

--- functions.php
+++ functions.php
@@ -1,1 +1,8 @@
-<?
+<?php
+/*
+ * Ici je crée des zones pour mes menu ainsi WP sais qu'il y a des menus.
+ * Première étape pour les menus
+ */
+register_nav_menus(array(
+    'main-menu' => "Description zone pour back-office"
+));
--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 
 
-<?php get_footer(); ?>
+<?php get_header() ?>
+
 
 <?php
-get_header() ?>
-<?php
-while (have_posts()) :
-    the_post();
+while (have_posts()) : the_post();
 ?>
-<h1><?php echo $post->post_title; ?></h1>
+
+    <h1><?php echo $post->post_title; ?></h1>
+
 <?php
 endwhile;
 ?>
+
+
 <?php get_footer() ?>
 
